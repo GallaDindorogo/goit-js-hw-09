be32540d51bf6317ab3438a861f3ade30baed28e
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2022-11-27 19:31:30 +0100

    third task-1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Form and its inputs
 const refs = {
   formEl: document.querySelector('.form'),
   delayEl: document.querySelector('input[name="delay"]'),
@@ -7,22 +8,15 @@ const refs = {
   amountEl: document.querySelector('input[name="amount"'),
 };
 
+// Event listener after submitting the form
 refs.formEl.addEventListener('submit', onCreatePromise);
 
-function onCreatePromise(e) {
-  e.preventDefault();
-  const delay = Number.parseInt(refs.delayEl.value, 10);
-  const step = Number.parseInt(refs.stepEl.value, 10);
-  const amount = Number.parseInt(refs.amountEl.value, 10);
-  createAllPromises(delay, step, amount);
-}
-
+// Loop to create promises from function createPromise
 function createAllPromises(delay, step, amount) {
   for (let i = 0 + 1; i <= amount; i++) {
     createPromise(i, delay).then(onResolve).catch(onReject);
     delay += step;
   }
-  // ---
 }
 
 function onResolve({ position, delay }) {
@@ -33,6 +27,16 @@ function onReject({ position, delay }) {
   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
 
+// Create promises after event listener
+function onCreatePromise(e) {
+  e.preventDefault();
+  const delay = Number.parseInt(refs.delayEl.value, 10);
+  const step = Number.parseInt(refs.stepEl.value, 10);
+  const amount = Number.parseInt(refs.amountEl.value, 10);
+  createAllPromises(delay, step, amount);
+}
+
+//function createPromise
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
